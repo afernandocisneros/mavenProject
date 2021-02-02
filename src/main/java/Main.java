19fdf998278cd798c849cf37a013d8b46e3bb470
afernandocisneros <fernando.cisnerosgaytan@gmail.com>
2021-02-02 00:03:05 -0600

    Testing integration Jenkins with Sonarqube

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args){
         System.out.println("MavenProject");
         System.out.println("Probando con la integracion de Jenkins con Sonarqube");
+        System.out.println("Mas pruebas");
     }
 
 }
